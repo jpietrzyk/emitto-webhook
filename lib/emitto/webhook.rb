717f89df8e786068131ef09cccd46ae6c59452b0
Jacek Pietrzyk <jacek.pietrzyk@netguru.com>
2023-07-17 01:27:52 +0200

    Initial comit

---
+++ lib/emitto/webhook.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "webhook/version"
+
+module Emitto
+  module Webhook
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/emitto/webhook/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Emitto
+  module Webhook
+    VERSION = "0.1.0"
+  end
+end
